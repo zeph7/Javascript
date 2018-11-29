27a3366ab594c2e30b4a24f054bcc7eaa59a8a7f
ashish7zeph <ashish199728@gmail.com>
2018-11-30 02:54:22 +0530

    Basics of javascript

---
+++ #2 Basics of JavaScript/#2.1_variables_and_constants.js
@@ -0,0 +1,37 @@
+// JavaScript Code for variables and constants
+
+
+	// use let or var keyword for declaring any variable
+
+
+// string variables
+let name = "Zephyr"
+var firstName = 'Calvin'
+var lastName = 'Klien'
+
+// Integer variables
+var score = 280
+let bonus = 20
+
+// float variables
+let f = 2.50
+
+console.log('String: ' + name)
+console.log('Integer: ' + score)
+console.log('Float: ' + f + '\n')
+
+// + for string concatenation
+let fullName = firstName + lastName
+console.log(fullName)
+
+// + for integer addition
+let total = score + bonus
+console.log(total)
+
+
+		// constant variable
+
+const place = 'USA'
+ // place = 'Canada' --- this statment give u an error
+ // as u r changing the value of a constant variable
+console.log(place)
---
+++ #2 Basics of JavaScript/#2.2_boolean_variables.js
@@ -0,0 +1,9 @@
+// JS Code for Boolean Variables
+
+	// bool variables can be either - true/false
+
+var _bool = true
+var grade = (30 > 300)
+
+console.log(_bool)
+console.log(grade)
