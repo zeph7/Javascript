f39ae969c4ead189b5ca8d9f7187d652b4d72985
ashish7zeph <ashish199728@gmail.com>
2018-11-30 02:53:12 +0530

    Introduction to javascript

---
+++ #1 Introduction to JavaScript/#1.1_hello_world.js
@@ -0,0 +1,6 @@
+// First JavaScript Code
+
+// JS is a client-side language
+// JS gives you the leniency, to add semicolon at the end of a statement or not
+
+console.log('Hello World');  // statement to print any string or variable
